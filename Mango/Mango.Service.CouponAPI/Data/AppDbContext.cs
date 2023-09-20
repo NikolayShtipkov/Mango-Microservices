c2c31828744bf5cb1d1995ad6ba3cbb283895c85
nikolayshtipkov <nikolayshtipkov@gmail.com>
2023-09-20 10:54:27 +0300

    install nuget packages and setup app database context

---
+++ Mango/Mango.Service.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Service.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Service.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Mango/Mango.Service.CouponAPI/Models/Coupon.cs
+++ Mango/Mango.Service.CouponAPI/Models/Coupon.cs
@@ -1,9 +1,14 @@
-﻿namespace Mango.Service.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Service.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
+        [Required]
         public string CouponCode { get; set; }
+        [Required]
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
     }
