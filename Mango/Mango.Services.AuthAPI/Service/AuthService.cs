f5b743c78551bfc0dcc40de831ad2ea766338697
nikolayshtipkov <nikolayshtipkov@gmail.com>
2023-09-25 15:12:07 +0300

    add jwt options and auth service

---
+++ Mango/Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,69 @@
+﻿using Mango.Service.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.DTO;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, 
+            RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public Task<LoginResponseDto> Login(LoginRequestDto requestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<string> Register(RegistrationRequestDto requestDto)
+        {
+            ApplicationUser user = new()
+            {
+                UserName = requestDto.Email,
+                Email = requestDto.Email,
+                NormalizedEmail = requestDto.Email.ToUpper(),
+                Name = requestDto.Name,
+                PhoneNumber = requestDto.PhoneNumber
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, requestDto.Password);
+                if (result.Succeeded)
+                {
+                    var userToReturn = _db.ApplicationUsers.First(u => u.UserName == requestDto.Email);
+
+                    UserDto userDto = new()
+                    {
+                        Email = userToReturn.Email,
+                        ID = userToReturn.Id,
+                        Name = userToReturn.Name,
+                        PhoneNumber = userToReturn.PhoneNumber
+                    };
+
+                    return "";
+                }
+                else
+                {
+                    return result.Errors.FirstOrDefault().Description;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return "Error encountered.";
+        }
+    }
+}
---
+++ Mango/Mango.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models.DTO;
+
+namespace Mango.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<string> Register(RegistrationRequestDto requestDto);
+        Task<LoginResponseDto> Login(LoginRequestDto requestDto);
+    }
+}
