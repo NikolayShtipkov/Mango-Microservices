e113f6fa71b471c8a8e0141bcd8b61cc3893bf49
nikolayshtipkov <nikolayshtipkov@gmail.com>
2023-09-26 13:03:17 +0300

    add auth service, interface and dto to mvc project

---
+++ Mango/Mango.Web/Service/AuthService.cs
@@ -0,0 +1,47 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using static Mango.Web.Utility.SD;
+
+namespace Mango.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = loginRequestDto,
+                Url = SD.CouponAPIBase + "/api/auth/login"
+            });
+        }
+
+        public async Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.CouponAPIBase + "/api/auth/register"
+            });
+        }
+
+        public async Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.CouponAPIBase + "/api/auth/assignRole"
+            });
+        }
+    }
+}
