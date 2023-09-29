7e59ddea1a12dec255917a4bed4689c06549916e
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-09-29 17:33:23 +0300

    add role authorization in coupon api

--- Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -77,6 +77,7 @@ namespace Mango.Services.CouponAPI.Controllers
         }
 
         [HttpPost]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Post([FromBody] CouponDto couponDTO)
         {
             try
@@ -97,6 +98,7 @@ namespace Mango.Services.CouponAPI.Controllers
         }
 
         [HttpPut]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Put([FromBody] CouponDto couponDTO)
         {
             try
@@ -118,6 +120,7 @@ namespace Mango.Services.CouponAPI.Controllers
 
         [HttpDelete]
         [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
         public ResponseDto Delete(int id)
         {
             try
