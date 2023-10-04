a97486b7a42ddf1b21d876bca5242d209cc65cce
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-10-04 15:33:23 +0300

    add models for shopping cart api

---
+++ Mango/Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.DTO;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+
+        [NotMapped]
+        public ProductDto Product { get; set; }
+    }
+}
