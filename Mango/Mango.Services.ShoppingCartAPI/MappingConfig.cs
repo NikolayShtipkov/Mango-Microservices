6ac020a2875ff560303632f1004c2e935f0fe102
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-10-05 10:08:33 +0300

    config shopping cart api project

---
+++ Mango/Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.DTO;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeader, CartHeaderDto>();
+                config.CreateMap<CartDetails, CartDetailsDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
