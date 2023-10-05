b332bde55753c90a1781f76db148de1d21d44e8a
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-10-05 10:18:54 +0300

    initialize shopping cart api controller

---
+++ Mango/Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+
+        public CartAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDto();
+        }
+
+        public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+        {
+            return _response;
+        }
+    }
+}
