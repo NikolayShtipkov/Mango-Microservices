14ac904bbf20636d56f0e622d90265251413ee37
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-10-16 13:51:50 +0300

    create message buss class library and add message bus class and interface

---
+++ Mango/Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        public Task PublishMessage(object message, string topic_queue_Name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
