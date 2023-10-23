c2882d2d92a4cd59ae849ac7f66503fbde7d7c5b
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-10-23 14:52:01 +0300

    configure mapping in order api project

--- Mango/Mango.Services.OrderAPI/MappingConfig.cs
+++ Mango/Mango.Services.OrderAPI/MappingConfig.cs
@@ -10,7 +10,17 @@ namespace Mango.Services.OrderAPI
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
-                
+                config.CreateMap<OrderHeaderDto, CartHeaderDto>()
+                .ForMember(dest => dest.CartTotal, u => u.MapFrom(src => src.OrderTotal)).ReverseMap();
+
+                config.CreateMap<CartDetailsDto, OrderDetailsDto>()
+                .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.Price, u => u.MapFrom(src => src.Product.Price));
+
+                config.CreateMap<OrderDetailsDto, CartDetailsDto>();
+
+                config.CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
+                config.CreateMap<OrderDetailsDto, OrderDetails>().ReverseMap();
             });
 
             return mappingConfig;
