e5dda4a995396c90992e66f43553ee1b5dad6100
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-10-23 13:38:21 +0300

    setup order api project

---
+++ Mango/Mango.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.DTO;
+
+namespace Mango.Services.OrderAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                
+            });
+
+            return mappingConfig;
+        }
+    }
+}
---
+++ Mango/Mango.Services.OrderAPI/Service/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using Mango.Services.OrderAPI.Models.DTO;
+using Mango.Services.OrderAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Mango.Services.OrderAPI.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            var client = _httpClientFactory.CreateClient("Product");
+            var response = await client.GetAsync($"/api/product");
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var res = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+            if (res.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(Convert.ToString(res.Result));
+            }
+
+            return new List<ProductDto>();
+        }
+    }
+}
