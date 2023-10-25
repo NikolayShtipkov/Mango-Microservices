740e8a5542e673ca12e31efdaffbb9d3f426a81d
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-10-25 12:23:35 +0300

    add stripe request dto for payments

---
+++ Mango/Mango.Services.OrderAPI/Models/DTO/StripeRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.OrderAPI.Models.DTO
+{
+    public class StripeRequestDto
+    {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSessionId { get; set; }
+        public string ApproveUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDto OrderHeader { get; set; }
+    }
+}
