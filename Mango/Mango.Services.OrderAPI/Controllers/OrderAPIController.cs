30cabaacb1a430588806ebac1ec850b470cd3e81
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-10-31 11:34:49 +0200

    add read/get endpoints in order api controller

--- Mango/Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Mango/Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Stripe.Checkout;
 using Stripe;
 using Mango.MessageBus;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.OrderAPI.Controllers
 {
@@ -32,6 +33,53 @@ namespace Mango.Services.OrderAPI.Controllers
             _messageBus = messageBus;
             _configuration = configuration;
             _response = new ResponseDto();
+        }
+
+        [Authorize]
+        [HttpGet("GetOrders")]
+        public ResponseDto? Get(string? userId = "")
+        {
+            try
+            {
+                IEnumerable<OrderHeader> objList;
+
+                if (User.IsInRole(SD.RoleAdmin))
+                {
+                    objList = _db.OrderHeaders.Include(u => u.OrderDetails).OrderByDescending(u => u.OrderHeaderId).ToList();
+                }
+                else
+                {
+                    objList = _db.OrderHeaders.Include(u => u.OrderDetails).Where(u => u.UserId == userId)
+                        .OrderByDescending(u => u.OrderHeaderId).ToList();
+                }
+
+                _response.Result = _mapper.Map<IEnumerable<OrderHeaderDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [Authorize]
+        [HttpGet("GetOrders/{id:int}")]
+        public ResponseDto? Get(int id)
+        {
+            try
+            {
+                OrderHeader orderHeader = _db.OrderHeaders.Include(u => u.OrderDetails).First(u => u.OrderHeaderId == id);
+                _response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
         }
 
         [Authorize]
