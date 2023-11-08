f8d3796e1aa062e780ee88ba3a6e55f8907d5681
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-11-08 11:23:52 +0200

    setup app to use authentication and ocelote

--- Mango/Mango.Gateway/Program.cs
+++ Mango/Mango.Gateway/Program.cs
@@ -1,6 +1,14 @@
+using Mango.Gateway.Extensions;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+builder.AddAppAuthentication();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.UseOcelot();
 app.Run();
