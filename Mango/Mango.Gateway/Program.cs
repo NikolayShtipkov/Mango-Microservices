09361e1325a1407ffc1b21e016fbe48ed75ab472
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-11-09 14:56:58 +0200

    setup ocelot.json file and add it in program.cs

--- Mango/Mango.Gateway/Program.cs
+++ Mango/Mango.Gateway/Program.cs
@@ -4,11 +4,13 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
 builder.AddAppAuthentication();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.UseOcelot().Wait();
+app.UseOcelot().GetAwaiter().GetResult();
 app.Run();
