838d457564d7571c72b91097f815e07012bc2d6f
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-11-14 13:11:06 +0200

    add RabbitMQAuthConsumer class in email api project

---
+++ Mango/Mango.Services.EmailAPI/Messaging/RabbitMQAuthConsumer.cs
@@ -0,0 +1,35 @@
+﻿using Mango.Services.EmailAPI.Service;
+using RabbitMQ.Client;
+
+namespace Mango.Services.EmailAPI.Messaging
+{
+    public class RabbitMQAuthConsumer : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly EmailService _emailService;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQAuthConsumer(IConfiguration configuration, EmailService emailService)
+        {
+            _configuration = configuration;
+            _emailService = emailService;
+
+            var factory = new ConnectionFactory 
+            { 
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(_configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue"), false, false, false, null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
