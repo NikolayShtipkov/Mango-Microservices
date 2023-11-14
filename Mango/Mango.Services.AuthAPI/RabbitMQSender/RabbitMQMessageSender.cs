a3da93fd7f2675ae4fc76a07fbcdf408e83307f6
Nikolay Shtipkov <NikolayShtipkov@gmail.com>
2023-11-14 12:32:57 +0200

    add rabbit mq message sender

---
+++ Mango/Mango.Services.AuthAPI/RabbitMQSender/RabbitMQMessageSender.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Mango.Services.AuthAPI.RabbitMQSender
+{
+    public class RabbitMQMessageSender : IRabbitMQAuthMessageSender
+    {
+        private readonly string _hostName;
+        private readonly string _username;
+        private readonly string _password;
+        private IConnection _connection;
+
+        public RabbitMQMessageSender()
+        {
+            _hostName = "localhost";
+            _username = "guest";
+            _password = "guest";
+        }
+
+        public void SendMessage(object message, string queueName)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostName,
+                UserName = _username,
+                Password = _password
+            };
+
+            _connection = factory.CreateConnection();
+            using var channel = _connection.CreateModel();
+            channel.QueueDeclare(queueName);
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+            channel.BasicPublish(exchange: "", routingKey: queueName, body: body);
+        }
+    }
+}
